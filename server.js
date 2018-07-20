27fa689d1de79fe2ece6e7d809bf1d9b855abdb8
Алексей Казаринов <alexey.kazarinov@gmail.com>
2018-07-20 10:12:34 +0300

    Server settings

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-var PeerServer = require('./lib/index').PeerServer;
-var server = new PeerServer({
-    port: process.env.PORT || 9000,
-    key: 'peerjs'
-});
+var express = require('express');
+var app = express();
+var ExpressPeerServer = require('peer').ExpressPeerServer;
+
+var server = app.listen(process.env.PORT || 9000);
+
+var options = {
+    debug: true
+};
+
+var peerserver = ExpressPeerServer(server, options);
+
+app.use('/api', peerserver);
+
+// options = app._options = util.extend({
+//     debug: false,
+//     timeout: 5000,
+//     key: 'peerjs',
+//     ip_limit: 5000,
+//     concurrent_limit: 5000,
+//     allow_discovery: false,
+//     proxied: false
+// }, options);
