dc38dfeb71d806cb17e15083ef373ed6bc12132b
Алексей Казаринов <alexey.kazarinov@gmail.com>
2018-07-20 12:23:18 +0300

    Server settings

--- server.js
+++ server.js
@@ -1,32 +1,26 @@
-var express = require('express');
-var app = express();
-var ExpressPeerServer = require('./lib/index').ExpressPeerServer;
+const express = require("express");
+const app = express();
+const ExpressPeerServer = require("./lib/index").ExpressPeerServer;
 
-var server = app.listen(process.env.PORT || 9000);
-var options = {};
-var peerserver = ExpressPeerServer(server, options);
+const server = app.listen(process.env.PORT || 9000);
+const options = {};
+const peerserver = ExpressPeerServer(server, options);
 
 app.use('/api', peerserver);
 
-var connected = [];
+const peers = [];
 peerserver.on('connection', function (id) {
-    var idx = connected.indexOf(id);
-    if (idx === -1) {connected.push(id);}
+    const idx = peers.indexOf(id);
+    if (idx === -1) {
+        peers.push(id);
+    }
 });
 peerserver.on('disconnect', function (id) {
-    var idx = connected.indexOf(id);
-    if (idx !== -1) {connected.splice(idx, 1);}
+    const idx = peers.indexOf(id);
+    if (idx !== -1) {
+        peers.splice(idx, 1);
+    }
 });
 app.get('/peers', function (req, res) {
-    return res.json(connected);
+    return res.json(peers);
 });
-
-// options = app._options = util.extend({
-//     debug: false,
-//     timeout: 5000,
-//     key: 'peerjs',
-//     ip_limit: 5000,
-//     concurrent_limit: 5000,
-//     allow_discovery: false,
-//     proxied: false
-// }, options);
