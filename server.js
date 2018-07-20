237581a728110bdc65ea79fa0fe5c46f9e275161
Алексей Казаринов <alexey.kazarinov@gmail.com>
2018-07-20 10:29:12 +0300

    Server settings

--- server.js
+++ server.js
@@ -3,14 +3,23 @@ var app = express();
 var ExpressPeerServer = require('./lib/index').ExpressPeerServer;
 
 var server = app.listen(process.env.PORT || 9000);
-
-var options = {
-    debug: true
-};
-
+var options = {};
 var peerserver = ExpressPeerServer(server, options);
 
 app.use('/api', peerserver);
+
+var connected = [];
+peerserver.on('connection', function (id) {
+    var idx = connected.indexOf(id);
+    if (idx === -1) {connected.push(id);}
+});
+peerserver.on('disconnect', function (id) {
+    var idx = connected.indexOf(id);
+    if (idx !== -1) {connected.splice(idx, 1);}
+});
+app.get('/peers', function (req, res) {
+    return res.json(connected);
+});
 
 // options = app._options = util.extend({
 //     debug: false,
